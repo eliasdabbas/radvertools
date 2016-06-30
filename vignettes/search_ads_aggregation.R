a41ae57beb8fcd256fcbbf2771de689428d02708
Elias <eliasdabbas@gmail.com>
2016-07-01 01:32:15 +0300

    fixes

---
+++ vignettes/search_ads_aggregation.R
@@ -0,0 +1,10 @@
+## ----message=FALSE, warning=FALSE, eval=FALSE----------------------------
+#  library(advertools)
+#  library(dplyr)
+#  library(tidyr)
+#  search_ads_google(keywords = c("flight reservations", "hotel booking"), domains = c("com", "co.uk"))
+
+## ------------------------------------------------------------------------
+library(advertools)
+head(sample_search_ads, 10)
+
