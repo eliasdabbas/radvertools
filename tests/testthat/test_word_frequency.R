bfbffd333ae933bb902b95858014a6afc1041019
Elias <eliasdabbas@gmail.com>
2016-07-01 23:03:55 +0300

    test making sure stop words are covered

--- tests/testthat/test_word_frequency.R
+++ tests/testthat/test_word_frequency.R
@@ -4,7 +4,7 @@ context("word_frequency")
 # Objects ----------------------------------------------------------------------
 
 keywords <- data.frame(
-  words = c("credit cards", "best credit cards", "credit card application", "personal loan"),
+  words = c("the credit cards", "best of credit cards", "credit card application", "personal loan"),
   clicks = c(100, 120, 110, 130)
 )
 word_freq <- word_frequency(keywords)
@@ -15,7 +15,7 @@ test_that("various character values are correctly in place", {
   expect_equivalent(word_freq$word[1], "credit")
   expect_equivalent(word_freq$word[7], "card")
   expect_equivalent(word_freq$text[1], as.factor("personal loan"))
-  expect_equivalent(word_freq$text[4], as.factor("credit cards"))
+  expect_equivalent(word_freq$text[4], as.factor("the credit cards"))
 })
 
 test_that("various numeric values are correctly in place", {
