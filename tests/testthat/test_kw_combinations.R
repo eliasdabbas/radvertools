97b2b9e31a5fca24109bc59b9d34052e0d76012b
Elias <eliasdabbas@gmail.com>
2016-07-01 22:05:49 +0300

    new test for kw_combinations

---
+++ tests/testthat/test_kw_combinations.R
@@ -0,0 +1,18 @@
+library(advertools)
+context("kw_combinations")
+kw_df <- data.frame(kw_1 = c("word", "thing", "something"),
+                         kw_2 = c("this", "that", "them"),
+                         kw_3 = c("good", "morning", "friend"))
+
+kw_df_combo <- kw_combinations(df = kw_df, cols = c("kw_1", "kw_2"))
+
+
+test_that("kw_combinations adds a new column", {
+  expect_equal(
+    ncol(kw_combinations(kw_df, c("kw_1", "kw_2"))) - ncol(kw_df),
+    1)
+})
+
+test_that("kw_combinations produces the right output", {
+  expect_equal(kw_df_combo[1,1], "word this")
+})
