09b26d45088e29f5bd620c2f18fad2e18c24220b
Elias <eliasdabbas@gmail.com>
2016-07-03 12:23:31 +0300

    new tests to cover urls

--- tests/testthat/test_twitter.R
+++ tests/testthat/test_twitter.R
@@ -3,9 +3,9 @@ context("twitter")
 
 # Objects ----------------------------------------------------------------------
 
-tweets <- c("this is my first #tweet with #two hashtags and one @mention",
-            "my second tweet has no hashtags and no mentions",
-            "my third #tweet #has #the most hashtags and @mentions @mention")
+tweets <- c("this is my first #tweet with #two hashtags and one @mention http://t.co/foo",
+            "my second tweet has no hashtags and no mentions http://bit.ly/fk47d https://t.co/bar",
+            "my third #tweet #has #the most hashtags and @mentions @mention http://sub.domain/baz123")
 
 tweet_plain_text <- c("this tweet has no hashtags or mentions",
                       "neither does this one",
@@ -15,6 +15,7 @@ tweets_hash <- twtr_get_hashtags(tweets)
 tweets_mention <- twtr_get_mentions(tweets)
 tweets_words <- twtr_get_words(tweets, c("hashtags", "and"), exact = T)
 tweets_words_notexact <- twtr_get_words(tweets, c("mention", "has"), exact = F)
+tweets_urls <- twtr_get_urls(tweets)
 
 # Tests ------------------------------------------------------------------------
 
@@ -23,6 +24,7 @@ test_that("tweets have correct number of hashtags & mentions",{
   expect_equal(sum(tweets_mention$mention_count$mention_count), 3)
   expect_equal(sum(tweets_words$word_count$word_count), 6)
   expect_equal(sum(tweets_words_notexact$word_count$word_count), 9)
+  expect_equal(sum(tweets_urls$url_count$url_count), 4)
 })
 
 test_that("tweets & mentions have correct dims", {
@@ -30,19 +32,22 @@ test_that("tweets & mentions have correct dims", {
   expect_equal(dim(tweets_mention$mentions), c(3,2))
   expect_equal(dim(tweets_words$words), c(3,2))
   expect_equal(dim(tweets_words_notexact$words), c(3,4))
+  expect_equal(dim(tweets_urls$urls), c(3,2))
 })
 
-test_that("3rd and 4th hashtag correctly identified", {
+test_that("certain values are correctly identified", {
   expect_equivalent(tweets_hash$hashtags[1,1], "#tweet")
   expect_equivalent(tweets_hash$hashtags[3,2], "#has")
   expect_equivalent(tweets_words$words[2,2], "and")
   expect_equivalent(tweets_words_notexact$words[1,2], "mention")
+  expect_equivalent(tweets_urls$urls[1,1], "http://t.co/foo")
 })
 
 test_that("tweets with no hashtags or mentions produce an error", {
   expect_error(twtr_get_hashtags(tweet_plain_text))
   expect_error(twtr_get_mentions(tweet_plain_text))
   expect_error(twtr_get_words(tweet_plain_text))
+  expect_error(twtr_get_urls(tweet_plain_text))
 })
 
 
